36e8ea9a0b19650eee41b84dc5ef73394a3a7aae
esraadelivr <esraa.abdelaziz@dlivr.io>
2022-03-14 11:30:29 +0200

    category migration and controller

---
+++ src/Entity/Category.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    const CATEGORY_FEATURED = "featured";
+    const CATEGORY_GENERAL = "general";
+    const CATEGORY_TRENDING = "trending";
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    public $name;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setType($type)
+    {
+        if (!in_array($type, array(self::CATEGORY_FEATURED, self::CATEGORY_GENERAL, self::CATEGORY_TRENDING))) {
+            throw new \InvalidArgumentException("Invalid type");
+        }
+        $this->type = $this->type;
+    }
+    public function getType(): string
+    {
+        return $this->type;
+    }
+}
